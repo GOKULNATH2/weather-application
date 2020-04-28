3ac7ae515c67a236d6f18cd165b62eb002587b35
Hugues RIME <qpfx5412@l-d4-be-d9-83-f3-a.rd.francetelecom.fr>
2020-04-28 09:58:25 +0200

    create simple map with input box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var ol: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'map-application';
+  latitude: number = 48.74441767602127;
+  longitude: number = -3.4510803222656246;
+
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+        zoom: 8
+      })
+    });
+
+
+  this.map.on('click', function (args) {
+    console.log(args.coordinate);
+    var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+    console.log(lonlat);
+  
+    var lon = lonlat[0];
+    var lat = lonlat[1];
+    //alert(`lat: ${lat} long: ${lon}`);
+  });
+  };
+  setCenter() {
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(8);
+  }
+  
 }
