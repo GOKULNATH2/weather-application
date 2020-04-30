037b38139e8c40d0593878c6d18c65848aaf188d
Hugues RIME <qpfx5412@l-d4-be-d9-83-f3-a.rd.francetelecom.fr>
2020-04-30 12:09:43 +0200

    global: replace openlayer by leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
-declare var ol: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,40 +7,4 @@ declare var ol: any;
 })
 export class AppComponent {
   title = 'map-application';
-  latitude: number = 48.74441767602127;
-  longitude: number = -3.4510803222656246;
-
-  map: any;
-
-  ngOnInit() {
-    this.map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
-        zoom: 8
-      })
-    });
-
-
-  this.map.on('click', function (args) {
-    console.log(args.coordinate);
-    var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
-    console.log(lonlat);
-  
-    var lon = lonlat[0];
-    var lat = lonlat[1];
-    //alert(`lat: ${lat} long: ${lon}`);
-  });
-  };
-  setCenter() {
-    var view = this.map.getView();
-    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
-    view.setZoom(8);
-  }
-  
 }
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,36 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements AfterViewInit {
+  private map;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+    //var marker = L.marker([51.5, -0.09]).addTo(this.map);
+     
+
+
+  }
+  
+}
