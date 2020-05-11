65d6e94ddfb9a0d9455c5aa266ac5fe5517019cd
Hugues RIME <qpfx5412@l-d4-be-d9-83-f3-a.rd.francetelecom.fr>
2020-05-11 16:25:54 +0200

    up keyboard move and zoom

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,6 +10,12 @@ export enum KEY_CODE {
   CENTER = 13
 }
 
+export enum CONST {
+  ZOOM_MAX = 18,
+  ZOOM_MIN = 2,
+  LAT_MAX = 85,
+}
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -19,14 +25,15 @@ export class MapComponent implements AfterViewInit {
   private map;
   private coodLat;
   private coodLon;
-  private zoom;
-  private moveMode=true;
+  private moveMode = true;
+  private handleZoom = 5;
+  private moveSize = 5;
+  public handleIcon = "move";
 
   constructor() {
-    this.coodLat=45;
-    this.coodLon=5;
-    this.zoom=5;
-   }
+    this.coodLat = 45;
+    this.coodLon = 5;
+  }
 
   ngAfterViewInit(): void {
     this.initMap();
@@ -37,90 +44,108 @@ export class MapComponent implements AfterViewInit {
     this.map = L.map('map', {
       zoomControl: false,
       center: [this.coodLat, this.coodLon],
-      zoom: this.zoom,
+      zoom: this.handleZoom,
     });
 
     L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+      //attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
+    // disable keyboard
+    this.map.keyboard.disable();
+
     //L.Control.geocoder().addTo(map);
     //L.Control.Geocoder.nominatim(options);
     //var marker = L.marker([51.5, -0.09]).addTo(this.map);
-    this.map.keyboard.disable();
 
     L.Control.geocoder({
       position: 'topleft',
       collapsed: false,
       placeholder: 'Search...',
       defaultMarkGeocode: true,
-    }).addTo(this.map); 
+    }).addTo(this.map);
   }
-    
+
   @HostListener('window:keyup', ['$event'])
-    keyEvent(event: KeyboardEvent) {
-      console.log(event.keyCode);
-      this.handlingMap(event.keyCode);
+  keyEvent(event: KeyboardEvent) {
+    console.log(this.map.getCenter());
+    console.log(this.map.getZoom());
+    this.handlingMap(event.keyCode);
   }
 
-  private handlingMap(keyCode): void{
+  private handlingMap(keyCode): void {
 
-    switch(keyCode){
+    switch (keyCode) {
       case KEY_CODE.UP_ARROW:
-        if(this.moveMode){
-          this.moveMap(1,0);
-        }else{
-          this.zoomMap(1);
+        if (this.moveMode) {
+          if (this.map.getCenter().lat < CONST.LAT_MAX) {
+            this.moveMap(1, 0);
+          }
+        } else {
+          if (this.handleZoom < CONST.ZOOM_MAX) {
+            this.zoomMap(1);
+            this.moveSize /= 2;
+          }
         }
-      break;
+        break;
       case KEY_CODE.DOWN_ARROW:
-        if(this.moveMode){
-          this.moveMap(-1,0);
-        }else{
-          this.zoomMap(-1);
+        if (this.moveMode) {
+          if (this.map.getCenter().lat > -CONST.LAT_MAX) {
+            this.moveMap(-1, 0);
+          }
+        } else {
+          if (this.handleZoom > CONST.ZOOM_MIN) {
+            this.zoomMap(-1);
+            this.moveSize *= 2
+          }
+
         }
-      break;
+        break;
       case KEY_CODE.RIGHT_ARROW:
-        if(this.moveMode){
-          this.moveMap(0,1);
-        }else{
+        if (this.moveMode) {
+          this.moveMap(0, 1);
+        } else {
         }
-      break;
+        break;
       case KEY_CODE.LEFT_ARROW:
-        if(this.moveMode){
-          this.moveMap(0,-1);
-        }else{
+        if (this.moveMode) {
+          this.moveMap(0, -1);
+        } else {
         }
-      break;
+        break;
       case KEY_CODE.CENTER:
         this.changeMode();
-      break;
+        break;
     }
   }
 
-  private changeMode(): void{
-    this.moveMode=!this.moveMode;
-    if(this.moveMode){
+  private changeMode(): void {
+    this.moveMode = !this.moveMode;
+    if (this.moveMode) {
+      this.handleIcon = "move";
       console.log("move");
-    }else{
+    } else {
+      this.handleIcon = "zoom";
       console.log("zoom");
     }
   }
 
-  private moveMap(lat,lon): void{
-    this.coodLat+=lat;
-    this.coodLon+=lon;
-    this.map.setView([this.coodLat, this.coodLon],this.zoom);
+  private moveMap(lat, lon): void {
+    // calcul new coordinates
+    this.coodLat += lat * this.moveSize;
+    this.coodLon += lon * this.moveSize;
+    this.map.setView([this.coodLat, this.coodLon], this.handleZoom);
   }
 
-  private zoomMap(zoom): void{
-    this.zoom+=zoom;
-    this.map.setZoom(this.zoom);
+  private zoomMap(zoom): void {
+    // update zoom
+    this.handleZoom += zoom;
+    this.map.setZoom(this.handleZoom);
   }
 
-  showSearch(){
-   /* this.show = !this.show;  
-    setTimeout(()=>{ // this will make the execution after the above boolean has changed
-      this.searchElement.nativeElement.focus();
-    },0); */ 
+  showSearch() {
+    /* this.show = !this.show;  
+     setTimeout(()=>{ // this will make the execution after the above boolean has changed
+       this.searchElement.nativeElement.focus();
+     },0); */
   }
 }
